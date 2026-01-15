5cb234a4d7df62b88a89daff85e8f951f2218e78
dvpenev-boop <dvpenev-boop@users.noreply.github.com>
2026-01-15 21:37:10 +0200

    Данни 51%

---
+++ EE Doklad/EE.Doklad/Views/ObjectDataSectionEditor.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace EE.Doklad.Views
+{
+    /// <summary>
+    /// Interaction logic for ObjectDataSectionEditor.xaml
+    /// </summary>
+    public partial class ObjectDataSectionEditor : UserControl
+    {
+        public ObjectDataSectionEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- EE Doklad/EE.Doklad/obj/Debug/net8.0-windows/MainWindow.g.cs
+++ EE Doklad/EE.Doklad/obj/Debug/net8.0-windows/MainWindow.g.cs
@@ -62,7 +62,7 @@ namespace EE.Doklad {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EE.Doklad;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EE.Doklad;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
